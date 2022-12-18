73b67b0e51ba5444c5a55f0b61e3cdea87e32f34
Mariam Khattab <116390513+N00ne-00@users.noreply.github.com>
2022-12-18 23:03:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        public static int find_perfectnumbers(int input)
+        {
+            int sum = 0;
+            for (int i = 1; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("please enter the first number : ");
+            int firstnum = int.Parse(Console.ReadLine());
+
+            Console.Write("please enter the second number : ");
+            int secondnum = int.Parse(Console.ReadLine());
+
+            for (int input = firstnum; input < secondnum; input++)
+            {
+                int result = find_perfectnumbers(input);
+                if (input == result)
+                {
+                    Console.WriteLine(input + " is perfect number");
+
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
