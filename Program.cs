e7a0c2c89cf688651550ac0872f7dc9a8adfffe4
Mariam Khattab <116390513+N00ne-00@users.noreply.github.com>
2022-12-19 02:01:30 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,41 +4,42 @@ namespace project_2
 {
     class Program
     {
-        public static int find_perfectnumbers(int input)
+        public static int find_perfectnumbers(int input) //فتحت داله جديده وعرفتها ب اي اسم 
         {
-            int sum = 0;
-            for (int i = 1; i < input; i++)
+            int sum = 0;                         // عرفت متغير الجمع واديله قيمه ابتدائيه صفر
+            for (int i = 1; i < input; i++)     //اعمل تكرار لكل الارقام اللي دخلت علشان اعمل شرط واعرف هل الرقم دا بيساوي مجموع قواسمه الموجبه ولا لا
             {
-                if (input % i == 0)
+                if (input % i == 0) // شرط عشان اتاكد ان ناتج القسمه ب صفر لو اه ساعتها اجمع الرقم علي متغير الجمع 
                 {
                     sum = sum + i;
                 }
             }
-            return sum;
+            return sum; //اخليه يرجع تاني للداله وينفذها
 
         }
 
 
 
-        static void Main(string[] args)
+        static void Main(string[] args) // دي الداله الاصليه 
         {
 
 
-            Console.Write("please enter the first number : ");
+            Console.Write("please enter the first number : "); // اسمح للمستخدم انه يدخل رقمين علشان احدد المجال بتاعي
             int firstnum = int.Parse(Console.ReadLine());
 
             Console.Write("please enter the second number : ");
             int secondnum = int.Parse(Console.ReadLine());
 
-            for (int input = firstnum; input < secondnum; input++)
+            for (int input = firstnum; input < secondnum; input++) //اعمل تكرار واعرف متغير واخليه يكون هو المجال كله ودا عن طريق اني اساويه بالرقم الاول واخليه اقل من الرقم التاني
             {
-                int result = find_perfectnumbers(input);
-                if (input == result)
+                int result = find_perfectnumbers(input); // اعرف متغير بالناتج واقوم بوضعه في الداله الاولي لكي اتاكد هل انطبقت عليه الداله لو اه يبقي عدد مثالي
+                if (input == result) // اعمل شلرط اتاكد ان الرقم دا موجود في الداله الاولي
                 {
-                    Console.WriteLine(input + " is perfect number");
+                    Console.WriteLine(input + " is perfect number"); // لو الرقم موجود وبيساويه هيكون رقم مثالي اعطي امر بالطباعه ويكون البرنامج جاهز
 
                 }
-
+                       // thank you :)
+                       //Mariam ramadan
 
             }
 
